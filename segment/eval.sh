f0b64ee4063701d8aa7ed3b9f73b69cd1da6922d
munib <munib@node1616.inband>
2025-11-07 15:13:08 -0500

    segment with a100 only

--- segment/eval.sh
+++ segment/eval.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=batch-segment
+#SBATCH --output=/home/munib/OUTPUT/g5ht-pipeline/segment/%j.out
 #SBATCH -n 1
 #SBATCH -p ou_bcs_normal
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=16G
-#SBATCH --output=/home/albert_w/outputs/slurm-%j.out
 
 module load miniforge
 conda activate eval
-python -u /home/albert_w/scripts/eval.py $1
+python -u /home/munib/CODE/g5ht-pipeline/segment/eval.py $1
